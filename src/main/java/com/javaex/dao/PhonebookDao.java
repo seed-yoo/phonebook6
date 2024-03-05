880c5a5d020aa2a9fb6cb2d4cb60e1b97d79b63b
seed-yoo <lukdown10@naver.com>
2024-03-05 18:04:02 +0900

    2024/03/05

--- src/main/java/com/javaex/dao/PhonebookDao.java
+++ src/main/java/com/javaex/dao/PhonebookDao.java
@@ -16,71 +16,71 @@ public class PhonebookDao {
 	private SqlSession sqlSession;
 
 	// 전체가져오기
-		public List<PersonVo> personSelect() {
-			System.out.println("PhonebookDao.personSelect()");
+	public List<PersonVo> personSelect() {
+		System.out.println("PhonebookDao.personSelect()");
 
-			List<PersonVo> personList = sqlSession.selectList("phonebook.select");
+		List<PersonVo> personList = sqlSession.selectList("phonebook.select");
 
-			// System.out.println(personList);
+		// System.out.println(personList);
 
-			return personList;
-		}
+		return personList;
+	}
 
-		// 등록
-		public int personInsert(PersonVo personVo) {
+	// 등록
+	public int personInsert(PersonVo personVo) {
 
-			int count = sqlSession.insert("phonebook.insert", personVo);
+		int count = sqlSession.insert("phonebook.insert", personVo);
 
-			return count;
-		}
-		
-		// 등록2
-		public int personInsert2(Map<String, String> pMap) {
-			System.out.println("PhonebookDao.personInsert2()");
-			System.out.println(pMap);
-			
-			int count = sqlSession.insert("phonebook.insert", pMap);
-			
-			return count;
-		}
+		return count;
+	}
 
-		// 1개 가져오기
-		public PersonVo personSelectOne(int no) {
-			System.out.println("PhonebookDao.personSelectOne()");
+	// 등록2
+	public int personInsert2(Map<String, String> pMap) {
+		System.out.println("PhonebookDao.personInsert2()");
+		System.out.println(pMap);
 
-			PersonVo personVo = sqlSession.selectOne("phonebook.selectOne", no);
-			System.out.println(personVo);
-			return personVo;
-		}
-		
-		// 1개 가져오기2
-		public Map<String, Object> personSelectOne2(int no) {
-			System.out.println("PhonebookDao.personSelectOne2()");
-			
-			Map<String, Object> pMap = sqlSession.selectOne("phonebook.selectOne2", no);
-			//System.out.println(pMap);
-			//System.out.println(pMap.get("name"));
-			
-			return pMap;
-		}
+		int count = sqlSession.insert("phonebook.insert", pMap);
 
-		// 수정
-		public int personModify(PersonVo personVo) {
-			System.out.println("PhonebookDao.personModify()");
-			
-			int count = sqlSession.update("phonebook.update", personVo);
-			
-			return count;
+		return count;
+	}
 
-		}
+	// 1개 가져오기
+	public PersonVo personSelectOne(int no) {
+		System.out.println("PhonebookDao.personSelectOne()");
 
-		// 삭제
-		public int personDelete(int no) {
+		PersonVo personVo = sqlSession.selectOne("phonebook.selectOne", no);
+		System.out.println(personVo);
+		return personVo;
+	}
 
-			int count = sqlSession.delete("phonebook.delete", no);
-			// System.out.println(count);
+	// 1개 가져오기2
+	public Map<String, Object> personSelectOne2(int no) {
+		System.out.println("PhonebookDao.personSelectOne2()");
 
-			return count;
-		}
+		Map<String, Object> pMap = sqlSession.selectOne("phonebook.selectOne2", no);
+		// System.out.println(pMap);
+		// System.out.println(pMap.get("name"));
+
+		return pMap;
+	}
+
+	// 수정
+	public int personModify(PersonVo personVo) {
+		System.out.println("PhonebookDao.personModify()");
+
+		int count = sqlSession.update("phonebook.update", personVo);
+
+		return count;
+
+	}
+
+	// 삭제
+	public int personDelete(int no) {
+
+		int count = sqlSession.delete("phonebook.delete", no);
+		// System.out.println(count);
+
+		return count;
+	}
 
 }
